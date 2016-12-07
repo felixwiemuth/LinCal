ed04c6b462b77d883f73c13ef43f8fe271776854
Felix Wiemuth <felixwiemuth@hotmail.de>
2016-12-07 20:45:13 +0100

    removed superfluous floating action button from EntryListActivity

--- app/src/main/java/felixwiemuth/lincal/ui/EntryListActivity.java
+++ app/src/main/java/felixwiemuth/lincal/ui/EntryListActivity.java
@@ -19,13 +19,10 @@ package felixwiemuth.lincal.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import felixwiemuth.lincal.R;
 
@@ -42,15 +39,6 @@ public class EntryListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_entry_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Action @ EntryDetail", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
