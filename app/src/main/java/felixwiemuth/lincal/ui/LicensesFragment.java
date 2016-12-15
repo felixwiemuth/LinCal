6c630502eecfb78c841ccc2e85bf261c60f12c63
Felix Wiemuth <felixwiemuth@hotmail.de>
2016-12-15 15:54:53 +0100

    removed unused code (concerning dialog close button)

--- app/src/main/java/felixwiemuth/lincal/ui/LicensesFragment.java
+++ app/src/main/java/felixwiemuth/lincal/ui/LicensesFragment.java
@@ -16,18 +16,11 @@
 
 package felixwiemuth.lincal.ui;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
-//If you don't support Android 2.x, you should use the non-support version!
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -37,49 +30,28 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.ProgressBar;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import felixwiemuth.lincal.R;
 
+//If you don't support Android 2.x, you should use the non-support version!
+
 /**
- * Created by Adam Speakman on 24/09/13.
- * http://speakman.net.nz
+ * Created by Adam Speakman on 24/09/13. http://speakman.net.nz
  */
 public class LicensesFragment extends DialogFragment {
 
     private AsyncTask<Void, Void, String> mLicenseLoader;
 
     private static final String FRAGMENT_TAG = "nz.net.speakman.androidlicensespage.LicensesFragment";
-    private static final String KEY_SHOW_CLOSE_BUTTON = "keyShowCloseButton";
 
-    /**
-     * Creates a new instance of LicensesFragment with no Close button.
-     *
-     * @return A new licenses fragment.
-     */
-    public static LicensesFragment newInstance() {
-        return new LicensesFragment();
-    }
-
-    /**
-     * Creates a new instance of LicensesFragment with an optional Close button.
-     *
-     * @param showCloseButton Whether to show a Close button at the bottom of the dialog.
-     *
-     * @return A new licenses fragment.
-     */
-    public static LicensesFragment newInstance(boolean showCloseButton) {
-        LicensesFragment fragment = new LicensesFragment();
-
-        Bundle bundle = new Bundle();
-        bundle.putBoolean(KEY_SHOW_CLOSE_BUTTON, showCloseButton);
-        fragment.setArguments(bundle);
-
-        return fragment;
-    }
 
     /**
      * Builds and displays a licenses fragment with no Close button. Requires
-     * "/res/raw/licenses.html" and "/res/layout/licenses_fragment.xml" to be
-     * present.
+     * "/res/raw/licenses.html" and "/res/layout/licenses_fragment.xml" to be present.
      *
      * @param fm A fragment manager instance used to display this LicensesFragment.
      */
@@ -92,28 +64,7 @@ public class LicensesFragment extends DialogFragment {
         ft.addToBackStack(null);
 
         // Create and show the dialog.
-        DialogFragment newFragment = LicensesFragment.newInstance();
-        newFragment.show(ft, FRAGMENT_TAG);
-    }
-
-    /**
-     * Builds and displays a licenses fragment with or without a Close button.
-     * Requires "/res/raw/licenses.html" and "/res/layout/licenses_fragment.xml"
-     * to be present.
-     *
-     * @param fm A fragment manager instance used to display this LicensesFragment.
-     * @param showCloseButton Whether to show a Close button at the bottom of the dialog.
-     */
-    public static void displayLicensesFragment(FragmentManager fm, boolean showCloseButton) {
-        FragmentTransaction ft = fm.beginTransaction();
-        Fragment prev = fm.findFragmentByTag(FRAGMENT_TAG);
-        if (prev != null) {
-            ft.remove(prev);
-        }
-        ft.addToBackStack(null);
-
-        // Create and show the dialog.
-        DialogFragment newFragment = LicensesFragment.newInstance(showCloseButton);
+        DialogFragment newFragment = new LicensesFragment();
         newFragment.show(ft, FRAGMENT_TAG);
     }
 
@@ -141,25 +92,9 @@ public class LicensesFragment extends DialogFragment {
         mWebView = (WebView) content.findViewById(R.id.licensesFragmentWebView);
         mIndeterminateProgress = (ProgressBar) content.findViewById(R.id.licensesFragmentIndeterminateProgress);
 
-        boolean showCloseButton = false;
-        Bundle arguments = getArguments();
-        if (arguments != null) {
-            showCloseButton = arguments.getBoolean(KEY_SHOW_CLOSE_BUTTON);
-        }
-
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("Open Source licenses");
+        builder.setTitle(R.string.menu_open_source_licenses);
         builder.setView(content);
-        if (showCloseButton) {
-            builder.setNegativeButton("Close",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.dismiss();
-                    }
-                });
-        }
-
         return builder.create();
     }
 
